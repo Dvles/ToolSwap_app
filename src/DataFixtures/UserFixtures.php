f85002b46a0b1ccabaa9a550cbe3ecee6dc9bc05
leslie <l.habimana@interface3.be>
2024-09-19 13:51:59 +0200

    User Fixture update

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -19,17 +19,55 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
-        $faker = Factory::create("fr_BE");
+        // Create realistic user data using multiple locales
+        $fakerBe = Factory::create("fr_BE");
+        $fakerRw = Factory::create("rw_RW");
+        $fakerIt = Factory::create("it_IT");
+        $fakerNl = Factory::create("nl_BE");
 
+        // 'users' to test web application
         for ($i = 1; $i < 6; $i++){
             $user = new User();
             $user->setEmail("user" . $i . "@gmail.com");
-            $user->setFirstName($faker->firstName());
-            $user->setLastName($faker->lastName());
+            $user->setFirstName($fakerBe->firstName());
+            $user->setLastName($fakerBe->lastName());
             $hashedPassword = $this->passwordHasher->hashPassword($user, '0000');
             $user->setPassword($hashedPassword);
             $user->setRoles(['ROLE_USER']);
             $user->setCommunity("Ixelles");
+            $manager->persist($user);
+        }
+
+        // 'realistic users' for display purposes 
+        // conditional to generate diverse country with multiple nationalities
+        for ($i = 1; $i < 50; $i++){
+            if($i % 5 === 0 && $i % 7 !== 0 && $i % 11 !== 0){
+                $firstName = $fakerNl->firstName();
+                $lastName = $fakerNl->lastName();
+                $email = ($firstName . '.' . $lastName . $i . "@gmail.com");
+            } elseif ($i % 7 === 0){
+                $firstName = $fakerRw->firstName();
+                $lastName = $fakerRw->lastName();
+                $email = ($firstName . '.' . $lastName . $i . "@gmail.com");
+            } elseif ($i % 11 === 0 ) {
+                $firstName = $fakerIt->firstName();
+                $lastName = $fakerIt->lastName();
+                $email = ($firstName . '.' . $lastName . $i . "@gmail.com");
+            } else {
+                $firstName = $fakerBe->firstName();
+                $lastName = $fakerBe->lastName();
+                $email = ($firstName . '.' . $lastName . $i . "@gmail.com");
+            }
+
+            $email = strtolower($email);
+            $user = new User();
+            $user->setEmail($email);
+            $user->setFirstName($firstName);
+            $user->setLastName($lastName);
+            $hashedPassword = $this->passwordHasher->hashPassword($user, '0000');
+            $user->setPassword($hashedPassword);
+            $user->setRoles(['ROLE_USER']);
+            $user->setCommunity($fakerBe->city());
             $manager->persist($user);
         }
 
