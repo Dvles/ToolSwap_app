122a8c176986987caf12b97021a6b54b0a52045b
Dvles <132080646+Dvles@users.noreply.github.com>
2024-09-22 16:11:36 +0200

    Update UML pdf as per renaming

--- migrations/Version20240922071327.php
+++ migrations/Version20240922071327.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240919122437 extends AbstractMigration
+final class Version20240922071327 extends AbstractMigration
 {
     public function getDescription(): string
     {
