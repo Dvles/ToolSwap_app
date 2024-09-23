bdfd9f38a57656a1bef81e35fa07c69320a6d1a2
Dvles <132080646+Dvles@users.noreply.github.com>
2024-09-23 18:42:41 +0200

    UML update

---
+++ entities.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Effacer les fichiers de migration
+rm -f migrations/V*
+
+# Effacer la BD
+symfony console doctrine:database:drop --force --no-interaction
+
+# Créer la BD
+symfony console doctrine:database:create
+
+# Créer une migration
+symfony console make:migration --no-interaction
+
+# Lancer la migration
+symfony console doctrine:migrations:migrate --no-interaction
+
--- migrations/Version20240923155406.php
+++ migrations/Version20240923155406.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240923094946 extends AbstractMigration
+final class Version20240923155406 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,7 +21,7 @@ final class Version20240923094946 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE borrow_tool (id INT AUTO_INCREMENT NOT NULL, user_borrower_id INT NOT NULL, tool_being_borrowed_id INT NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, status VARCHAR(255) NOT NULL, INDEX IDX_A3DE04F4B8D5F8BE (user_borrower_id), INDEX IDX_A3DE04F42F823257 (tool_being_borrowed_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE lender_review (id INT AUTO_INCREMENT NOT NULL, rating INT NOT NULL, comments LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE lender_review (id INT AUTO_INCREMENT NOT NULL, user_leaving_review_id INT NOT NULL, user_being_reviewed_id INT NOT NULL, rating INT NOT NULL, comments LONGTEXT DEFAULT NULL, INDEX IDX_14FFCC36DCAFF04B (user_leaving_review_id), INDEX IDX_14FFCC36F5F6B329 (user_being_reviewed_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tool (id INT AUTO_INCREMENT NOT NULL, user_of_tool_id INT NOT NULL, tool_availability_id INT DEFAULT NULL, tool_category_id INT NOT NULL, name VARCHAR(50) NOT NULL, description LONGTEXT DEFAULT NULL, tool_condition VARCHAR(20) NOT NULL, availability TINYINT(1) DEFAULT NULL, price_day NUMERIC(5, 2) DEFAULT NULL, image_tool VARCHAR(255) NOT NULL, INDEX IDX_20F33ED1D5691591 (user_of_tool_id), UNIQUE INDEX UNIQ_20F33ED1A1B75CA6 (tool_availability_id), INDEX IDX_20F33ED1887483BC (tool_category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tool_availability (id INT AUTO_INCREMENT NOT NULL, available_dates LONGTEXT NOT NULL COMMENT \'(DC2Type:simple_array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tool_category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -30,6 +30,8 @@ final class Version20240923094946 extends AbstractMigration
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE borrow_tool ADD CONSTRAINT FK_A3DE04F4B8D5F8BE FOREIGN KEY (user_borrower_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE borrow_tool ADD CONSTRAINT FK_A3DE04F42F823257 FOREIGN KEY (tool_being_borrowed_id) REFERENCES tool (id)');
+        $this->addSql('ALTER TABLE lender_review ADD CONSTRAINT FK_14FFCC36DCAFF04B FOREIGN KEY (user_leaving_review_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE lender_review ADD CONSTRAINT FK_14FFCC36F5F6B329 FOREIGN KEY (user_being_reviewed_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE tool ADD CONSTRAINT FK_20F33ED1D5691591 FOREIGN KEY (user_of_tool_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE tool ADD CONSTRAINT FK_20F33ED1A1B75CA6 FOREIGN KEY (tool_availability_id) REFERENCES tool_availability (id)');
         $this->addSql('ALTER TABLE tool ADD CONSTRAINT FK_20F33ED1887483BC FOREIGN KEY (tool_category_id) REFERENCES tool_category (id)');
@@ -42,6 +44,8 @@ final class Version20240923094946 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE borrow_tool DROP FOREIGN KEY FK_A3DE04F4B8D5F8BE');
         $this->addSql('ALTER TABLE borrow_tool DROP FOREIGN KEY FK_A3DE04F42F823257');
+        $this->addSql('ALTER TABLE lender_review DROP FOREIGN KEY FK_14FFCC36DCAFF04B');
+        $this->addSql('ALTER TABLE lender_review DROP FOREIGN KEY FK_14FFCC36F5F6B329');
         $this->addSql('ALTER TABLE tool DROP FOREIGN KEY FK_20F33ED1D5691591');
         $this->addSql('ALTER TABLE tool DROP FOREIGN KEY FK_20F33ED1A1B75CA6');
         $this->addSql('ALTER TABLE tool DROP FOREIGN KEY FK_20F33ED1887483BC');
