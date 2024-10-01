4492f2b5181d83b22d300c699b1e559e928d99f6
leslie <l.habimana@interface3.be>
2024-10-01 16:29:42 +0200

    circular ref fix 20 %

--- src/Controller/CalendarTestController.php
+++ src/Controller/CalendarTestController.php
@@ -22,24 +22,33 @@ class CalendarTestController extends AbstractController
     #[Route('/display/tool/calendar', name: 'display_tool_calendar')]
     public function afficherCalendrierUtilisateur(SerializerInterface $serializer): Response
     {
-
-        
         $user = $this->getUser(); 
         if (is_null($user)) {
             return $this->redirectToRoute("app_login");
         }
-
-        
+    
+        // Get tool availabilities for the user
         $toolAvailabilities = $user->getToolAvailabilities();
         
-        // Redirect if there are no availabilities
+        // Debug: Check if there are any availabilities
         if ($toolAvailabilities->isEmpty()) {
+            // Optionally add a message to inform user no availabilities found
             return $this->redirectToRoute("app_login"); 
         }
-        dd($toolAvailabilities);
         
-        $toolAvailabilitiesJSON = $serializer->serialize($toolAvailabilities, 'json', [AbstractNormalizer::IGNORED_ATTRIBUTES => ['user', 'tool', 'tool_availability']]);
+        // Debugging output to check the availabilities
+        // dd($toolAvailabilities); // Check what is being returned
+    
+        // Serialize the availabilities for the JSON response
+        $toolAvailabilitiesJSON = $serializer->serialize(
+            $toolAvailabilities, 
+            'json', 
+            [AbstractNormalizer::IGNORED_ATTRIBUTES => ['user', 'tool', 'toolAvailabilities']]
+        );
+    
+        // Prepare variables for rendering
         $vars = ['toolAvailabilitiesJSON' => $toolAvailabilitiesJSON];
         return $this->render('calendar_test/display_tool_availabilities.html.twig', $vars);
     }
+    
 }
