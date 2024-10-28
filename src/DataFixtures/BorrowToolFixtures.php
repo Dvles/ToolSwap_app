c765449f2860e757b0d88704338c988973faddae
Dvles <132080646+Dvles@users.noreply.github.com>
2024-10-28 13:48:18 +0100

    BorrowTool fixtures for pastDates only

--- src/DataFixtures/BorrowToolFixtures.php
+++ src/DataFixtures/BorrowToolFixtures.php
@@ -4,33 +4,55 @@ namespace App\DataFixtures;
 
 use App\Entity\BorrowTool;
 use App\Entity\ToolAvailability;
+use App\Entity\User;
+use App\Enum\ToolStatusEnum;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class BorrowToolFixtures extends Fixture
+class BorrowToolFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        
-        // $repToolAvailability = $manager->getRepository(ToolAvailability::class);
-        // //$toolAvailability = $rep
-        
+        $repToolAvailability = $manager->getRepository(ToolAvailability::class);
+        $toolAvailabilities = $repToolAvailability->findAll();
 
-        // for{$i = 0; $i < 20; $i++}{
-        //     $borrowTool = new BorrowTool();
-        //     $borrowTool->setRating();
-        //     $borrowTool->setComment()
+        $repUsers = $manager->getRepository(User::class);
+        $users = $repUsers->findAll();
 
+        // Ensure there are users to borrow tools from
+        if (empty($users)) {
+            throw new \Exception("No users found for borrowing.");
+        }
 
-        // };
-        
-        // // $product = new Product();
-        // // $manager->persist($product);
+        // Random borrowTools
+        foreach ($toolAvailabilities as $toolAvailability) {
+            // Get the owner of the tool
+            $owner = $toolAvailability->getUser();
 
-        // $manager->flush();
+            // Select a random user that is not the owner
+            $user = $owner;
+            do {
+                $randomUserIndex = array_rand($users); // Get a random index
+                $user = $users[$randomUserIndex];
+            } while ($user === $owner); // Ensure selected user is not the owner
+
+            // Check if the tool availability is not available
+            if (!$toolAvailability->isAvailable()) { 
+                $borrowTool = new BorrowTool();
+                $borrowTool->setStartDate($toolAvailability->getStart());
+                $borrowTool->setEndDate($toolAvailability->getEnd());
+                $borrowTool->setToolAvailability($toolAvailability); 
+                $borrowTool->setToolBeingBorrowed($toolAvailability->getTool());
+                $borrowTool->setUserBorrower($user);
+                $borrowTool->setStatus(ToolStatusEnum::COMPLETED);
+
+                $manager->persist($borrowTool); 
+            }
+        }
+
+        $manager->flush(); 
     }
-
-
 
     public function getDependencies()
     {
