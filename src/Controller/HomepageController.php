913c7cfa3e9790b2efbbcf2e81283eb6a387314a
Dvles <132080646+Dvles@users.noreply.github.com>
2024-11-22 00:09:07 +0100

    view enhancements homepage, login, register

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\BorrowToolRepository;
+use App\Repository\ToolRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +13,34 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index( ManagerRegistry $doctrine, ToolRepository $toolRepository, BorrowToolRepository $borrowToolRepository): Response
     {
-        return $this->render('homepage/index.html.twig');
+        $user = $this->getUser();
+        $tools = $user->getToolsOwned();
+        // repository method to get the count
+        $borrowedToolsCount = $borrowToolRepository->countBorrowedToolsByBorrower($user);
+        $lentToolsCount = $borrowToolRepository->countBorrowedToolsByOwner($user);
+        $toolsOwnedByOwnerCount = $toolRepository->countToolsOwnedByOwner($user);
+        $freeToolsOwnedByOwnerCount = $toolRepository->countFreeToolsOwnedByOwner($user);
+
+
+        $reviews = $user->getReviewsReceived();
+        $vars = [
+            'user' => $user,
+            'tools' => $tools,
+            'reviews' => $reviews,
+            'borrowedToolsCount' => $borrowedToolsCount,
+            'toolsOwnedByOwnerCount' => $toolsOwnedByOwnerCount,
+            'freeToolsOwnedByOwnerCount' => $freeToolsOwnedByOwnerCount,
+            'lentToolsCount' => $lentToolsCount
+
+        ];
+
+        //dd($user);
+        //dd($tools);
+        //dd($reviews);
+        
+        return $this->render('homepage/index.html.twig', $vars);
     }
 
     #[Route('/coming/soon', name: 'coming_soon')]
